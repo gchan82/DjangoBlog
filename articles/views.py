8604beb2d80575a88c9872ab3b07cd32ce53ed3e
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-11 12:28:07 -0800

    eof#16 article detail

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
-from django.http import HttpResponse
 
 # Create your views here.
 def article_list(request):
@@ -8,4 +8,6 @@ def article_list(request):
   return render(request, 'articles/article_list.html', {'articles': articles})
 
 def article_detail(request, slug):
-  return HttpResponse(slug)
+  # return HttpResponse(slug)
+  article = Article.objects.get(slug=slug)
+  return render(request, 'articles/article_detail.html', {'article': article})
