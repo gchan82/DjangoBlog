0817e4842da48a5e2344bbbacb9acbd9e8f6dde3
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-11 13:17:51 -0800

    eof#17: upload media images

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,7 @@ class Article(models.Model):
   body = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
   # add in thumbnail later
+  thumb = models.ImageField(default='default.png', blank=True)
   # add in author later
 
   def __str__(self):
